9b09b74dda731c7f7424105610c3d0ec7c3b961e
bluMyst <weirdusername@gmail.com>
2016-05-01 04:30:48 -0500

    Added main.js and javascript directory.

---
+++ javascript/main.js
@@ -0,0 +1,9 @@
+var harvester = require('role.harvester');
+var creep;
+
+module.exports.loop = function () {
+	for (var name in Game.creeps) {
+	    creep = Game.creeps[name];
+	    harvester.run(creep);
+	}
+}
